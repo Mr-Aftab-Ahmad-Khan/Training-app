948d4da4ae9d80dbd4a28e1b06a00bf3da7950ec
Zain-Chaudhary <chaudharyzain6206@gmail.com>
2024-05-27 01:50:18 -0700

    None

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-import 'react-bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import HeaderInfo from './Components/Header-Info/HeaderInfo';
+import logo from "./logo.svg";
+/////////////////////////////////////////////// importing Lib and modules
+import "./App.css";
+import "react-bootstrap";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+/////////////////////////////////////////////// importing components and pages
+import HeaderInfo from "./Components/Header-Info/HeaderInfo";
+import FeaturedCourses from "./Components/FeaturedCourses/FeaturedCourses";
+import Navbar from "./Components/Navbar/Navbar";
+import Home from "./Pages/Home/Home";
+import ContactUs from "./Pages/Contact/ContactUs";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <HeaderInfo/>
-      </header>
+      <HeaderInfo />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={""} />
+          <Route path="/about" element={""} />
+          <Route path="/services" element={""} />
+          <Route path="/contact" element={""} />
+        </Routes>
+      </Router>
+      {/* <Home /> */}
+      <ContactUs />
     </div>
   );
 }
